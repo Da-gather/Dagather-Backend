7e5c34841c57828628cd2a5571a5d64b5344d441
minjikim <inkmjj@sookmyung.ac.kr>
2023-04-28 01:34:23 +0900

    initial commit

---
+++ src/main/java/kr/org/dagather/DagatherApplication.java
@@ -0,0 +1,13 @@
+package kr.org.dagather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DagatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/org/dagather/DagatherApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.org.dagather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
