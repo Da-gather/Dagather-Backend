ac612f2cefda6f88d61b35ca38720f01008bcfb3
minjikim <inkmjj@sookmyung.ac.kr>
2023-05-17 17:03:24 +0900

    [add] auth filter 추가

---
+++ src/main/java/kr/org/dagather/common/filter/AuthFilter.java
@@ -0,0 +1,34 @@
+package kr.org.dagather.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@Order(0)
+public class AuthFilter extends OncePerRequestFilter {
+
+	@Getter
+	public static String currentMemberId;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws IOException, ServletException {
+
+		currentMemberId = request.getHeader("Authorization");
+		filterChain.doFilter(request, response);
+	}
+}
